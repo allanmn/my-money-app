8799f97ba463ec7fe2c8192d33fbd18cc3ee2554
Allan Fernando Marchek Neves <86494359+allanmn@users.noreply.github.com>
2023-09-09 15:37:59 -0300

    dashboard with redux

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,18 +1,56 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import ContentHeader from "../commom/template/content-header";
 import Content from "../commom/template/content";
+import ValueBox from "../commom/widgets/value-box";
+import Row from "../commom/layout/row";
+import { bindActionCreators } from "redux";
+import { getSummary } from "./dashboard-actions";
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                <ContentHeader title='Dashboard' small='Versão 1.0'/>
-                <Content>
-                    Dashboard
-                </Content>
-            </div>
-        );
-    }
+  componentDidMount() {
+    this.props.getSummary();
+  }
+
+  render() {
+    const { credit, debt } = this.props.summary;
+
+    return (
+      <div>
+        <ContentHeader title="Dashboard" small="Versão 1.0" />
+        <Content>
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value={`R$ ${credit}`}
+              text="Total de Créditos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value={`R$ ${debt}`}
+              text="Total de Débitos"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value={`R$ ${credit - debt}`}
+              text="Valor consolidado"
+            />
+          </Row>
+        </Content>
+      </div>
+    );
+  }
 }
 
-export default Dashboard;
+const mapStateProps = (state) => ({ summary: state.dashboard.summary });
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ getSummary }, dispatch);
+
+export default connect(mapStateProps, mapDispatchToProps)(Dashboard);
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./main/app";
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import reducers from "./main/reducers";
+import promise from 'redux-promise';
+import { applyMiddleware } from "redux";
+
+const store = applyMiddleware(promise)(createStore)(reducers);
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, 
+document.getElementById("app"));
